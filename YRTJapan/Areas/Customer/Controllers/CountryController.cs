ec7630112b574e36de63a49780644c23df6e490a
Tahir Samee <72864292+TahirSamee@users.noreply.github.com>
2025-06-23 00:50:01 +0500

    zambia page added

---
+++ YRTJapan/Areas/Customer/Controllers/CountryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YRTJapan.UI.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CountryController : Controller
+    {
+        public IActionResult Zambia()
+        {
+            return View();
+        }
+    }
+}
